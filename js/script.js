2324bbfc13d25d26e5eaf9d5ff8cc889161eff2b
Marco <marco.cusenza.me@gmail.com>
2021-11-10 17:15:06 +0100

    fine e funziona tutto

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const mailRegistrate = ["pippo@gmail.com", "pluto@gmail.com", "paperino@outlook.
 const userMail = prompt("Inserisci la tua email");
 const login = document.getElementById("login");
 const btnAgain = document.getElementById("again");
+let username = "";
 
 let valid = false;
 
@@ -14,34 +15,31 @@ for (let i = 0; i < mailRegistrate.length; i++) {
 if (valid == false) {
     login.innerHTML = `${userMail} NON sei un utente registrato.<br>Non puoi accedere al gioco.`
 } else {
-    const username = prompt("Sei un utente registrato! Inserisci il tuo username");
+    username = prompt("Sei un utente registrato! Inserisci il tuo username");
     document.getElementById("welcome").innerHTML = `Benvenuto ${username}, che il gioco dei dadi abbia inizio!`
 
     document.getElementById("content").classList.remove("ms_hidden");
-    giocoDadi();
-    btnAgain.addEventListener("click", giocoDadi());
+
+    btnAgain.addEventListener('click', function giocoDadi() {
+        let numCPU = Math.floor(Math.random() * 6) + 1;
+        console.log(numCPU);
+        document.getElementById("numCPU").innerHTML = numCPU;
+
+        let numUser = Math.floor(Math.random() * 6) + 1;
+        console.log(numUser);
+        document.getElementById("numUser").innerHTML = numUser;
+
+        const result = document.getElementById("result");
+        if (numCPU > numUser) {
+            console.log("Vince CPU!");
+            result.innerHTML = "Hai perso :("
+        } else if (numCPU == numUser) {
+            console.log("Pareggio!");
+            result.innerHTML = "Pareggio!"
+        } else {
+            console.log("Vince " + username + "!");
+            result.innerHTML = "Hai vinto! :D"
+        }
+    });
 }
 
-
-
-function giocoDadi() {
-    let numCPU = Math.floor(Math.random() * 6) + 1;
-    console.log(numCPU);
-    document.getElementById("numCPU").innerHTML = numCPU;
-
-    let numUser = Math.floor(Math.random() * 6) + 1;
-    console.log(numUser);
-    document.getElementById("numUser").innerHTML = numUser;
-
-    const result = document.getElementById("result");
-    if (numCPU > numUser) {
-        console.log("Vince CPU!");
-        result.innerHTML = "Hai perso :("
-    } else if (numCPU == numUser) {
-        console.log("Pareggio!");
-        result.innerHTML = "Pareggio!"
-    } else {
-        console.log("Vince " + username + "!");
-        result.innerHTML = "Hai vinto! :D"
-    }
-}
